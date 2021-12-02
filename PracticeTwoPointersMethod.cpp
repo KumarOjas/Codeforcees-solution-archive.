d83cea1918bb9b09eb0b25ca15cc73bdb67781fa
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-01 22:39:06 -0800

    Create PracticeTwoPointersMethod.cpp

---
+++ PracticeTwoPointersMethod.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <vecor>
+#include <set>
+typedef long long li;
+
+int main () {
+ 
+     ll n, k; scanf("%lld %lld", &n, &k);
+    std::vector<ll> a(n); for(ll p = 0; p < n; p++){scanf("%lld", &a[p]);}
+    std::multiset<ll> s;
+    ll left(0), cnt(0);
+    for(ll p = 0; p < n; p++){
+        s.insert(a[p]);
+        while((*(s.rbegin())) - (*(s.begin())) > k){s.erase(s.find(a[left])); ++left;}
+        cnt += p - left + 1;
+    }
+
+    printf("%lld\n", cnt);
+}
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
