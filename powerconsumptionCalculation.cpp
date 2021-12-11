605340b9906e58929b701884f9221cdbb727da70
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-10 23:34:10 -0800

    Create powerconsumptionCalculation.cpp

---
+++ powerconsumptionCalculation.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+
+int main () {
+  
+  int n(0),p1(0),p2(0),p3(0),T1(0),T2(0);
+  scanf("%d %d %d %d %d %d", &n, &P1, &P2, &P3, &T1, &T2);
+  
+   long total(0), previousTime(-1);
+
+   for(int k = 0; k < n; k++){
+
+        int start(0), finish(0); scanf("%d %d", &start, &finish);
+        if(previousTime < 0){previousTime = start;}
+        total += P1 * (finish - start);
+
+        int timeIdle = start - previousTime;
+        if(timeIdle > T1 + T2){total += (timeIdle - T1 - T2) * P3; timeIdle = T1+ T2;}
+        if(timeIdle > T1){total += (timeIdle - T1) * P2; timeIdle = T1;}
+        total += timeIdle * P1;
+
+        previousTime = finish;
+    }
+
+    printf("%ld\n", total);
+
+    return 0;
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
