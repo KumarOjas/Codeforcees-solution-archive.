4ea2e3ee6b53bdb74b7bf0bad23a5e54e159faaf
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-12 06:05:08 -0800

    Create TeamCompositionProgrammersandmathematicians.cpp

---
+++ TeamCompositionProgrammersandmathematicians.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+int main () {
+  
+  long t;
+  scanf("%ld",&t);
+  while (t--)
+  {
+    long a,b;
+    scanf("%ld %ld",&a,&b);
+    long res =(a<b ? a:b);
+    res = (res < (a+b)/4)?res:(a+b)/4;
+    printf("%ld\n",res);
+  }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
