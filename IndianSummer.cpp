ba3f2e3b203ea8df5cbd65cc4b248873f3279408
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2021-12-25 12:51:09 +0530

    Create IndianSummer.cpp

---
+++ IndianSummer.cpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+
+#include <iostream>
+
+#include <stding>
+
+#include <set>
+
+
+int main() {
+  
+  int n;
+
+  std:: cin >> n;
+  
+  
+    std::set<std::pair<std::string, std::string> > leaves;
+  
+  while(n--)
+  {
+  
+    std::string s1, s2; std::cin >> s1 >> s2;
+    
+    leaves.insert(std::pair<std::string, std::string>(s1, s2));
+    
+  }
+
+    std::cout << leaves.size() << std::endl;
+
+    return 0;
+}
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
