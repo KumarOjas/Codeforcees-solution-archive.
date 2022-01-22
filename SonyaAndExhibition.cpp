5f942b6d9ae82bcdcad193e738654721eb0da31a
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-01-22 18:22:36 +0530

    Create SonyaAndExhibition.cpp

---
+++ SonyaAndExhibition.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+
+
+
+int main (){
+  
+  
+    long n;
+  
+  
+    scanf("%ld", &n);
+    
+  
+  
+  for(long p = 0; p < n; p++){putchar(p & 1 ? '1' : '0');}
+  
+  
+  puts("");
+
+  
+  
+  return 0;
+}
