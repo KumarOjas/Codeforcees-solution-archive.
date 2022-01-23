4500b474b3b7403b6eee6950eae29a440c11dab8
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-01-23 19:01:37 +0530

    Create PieOrdie.cpp

---
+++ PieOrdie.cpp
@@ -0,0 +1,36 @@
+
+#include <cstdio>
+
+
+int main ()
+
+{
+   int n,m,k;
+ 
+  
+  scanf ("%d%d\n"&n,&m,&k);
+  
+  
+  for (int p=0;p<k;p++)
+  
+  {
+  
+    
+    int x,y;
+    
+    
+    scanf("%d %d\n", &x, &y);
+    
+     if(x <= 5 || x + 4 >= n || y <= 5 || y + 4 >= m){puts("YES"); return 0;}
+    }
+
+    puts("NO");
+
+    return 0;
+}
+    
+  
+  
+  
+  
+  
