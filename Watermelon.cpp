30ad512a462ddfb58b2aae9fe98f7adb9ce95ba4
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-01-23 18:35:45 +0530

    Create Watermelon.cpp

---
+++ Watermelon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main () {
+  
+  int w=0;
+   std::cin >> w ;
+  
+  
+  std::string output = (w <= 2 || w % 2)   ? "NO" : "YES";
+   
+  
+  std::cout << output << std::endl;
+  
+  
+  return 0;
+}
