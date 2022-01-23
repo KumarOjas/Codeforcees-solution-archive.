29840d1441484ff67cc472b74cbc9c439348155a
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-01-23 20:14:32 +0530

    Create ChatServersOutgoingTraffic.cpp

---
+++ ChatServersOutgoingTraffic.cpp
@@ -0,0 +1,34 @@
+#include <isotream>
+int main (){
+  
+   std::string line;
+ 
+  
+     int users(0), total(0);
+   
+  
+  while(getline(std::cin, line))
+  {
+  
+    
+    if(line[0] == '+')
+    {
+      ++users;
+    }
+        else if
+          
+          (line[0] == '-')
+        {
+          --users;
+        }
+        else{total += users * (line.size() - line.find(':') - 1);}
+    }
+
+    std::cout <<  total << std::endl;
+    return 0;
+}
+  
+  
+  
+  
+  
