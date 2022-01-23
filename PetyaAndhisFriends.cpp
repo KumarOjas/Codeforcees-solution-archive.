0a12ebc030136229ae62d958116e56af54168d9f
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-01-23 22:22:15 +0530

    Create PetyaAndhisFriends.cpp

---
+++ PetyaAndhisFriends.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+int main () {
+  
+  long n;
+  
+  
+    scanf ("%ld",&n);
+    
+  
+  if(n == 2)
+  {
+    puts("-1");
+  }
+  
+  else{printf("6\n10\n15\n"); 
+       
+       for(long p = 3; p < n; p++)
+       {
+         printf("%ld\n",6 * p);
+       }
+      }
+
+    return 0;
+}
