ad69547177ed59821ffd78e70ef5d58decc7035e
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-01-24 15:36:24 +0530

    Create Plugin.cpp

---
+++ Plugin.cpp
@@ -0,0 +1,26 @@
+#include <iosteram>
+int main () {
+  
+    std::string input ;
+  
+   getline(std::cin, input);
+   
+  
+    std::string output("");
+
+    for(int k = 0; k < input.size(); k++){
+    
+      
+      if(output.empty() || output.back() != input[k]){output += input[k];}
+      
+      
+      else{output.pop_back();}
+    }
+
+    
+  std::cout << output << std::endl;
+    return 0;
+}
+  
+  
+  
