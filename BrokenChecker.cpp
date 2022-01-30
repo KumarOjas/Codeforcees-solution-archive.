395b2db8d848f06d992a354e25d6c72cb6d0d735
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-01-30 13:03:01 +0530

    Create BrokenChecker.cpp

---
+++ BrokenChecker.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <iostream>
+
+int main () {
+  
+  std::string output("023121");
+ 
+  
+   int n(0); 
+  
+    scanf("%d", &n);
+    
+  
+    printf("%c\n", output[n]);
+
+    return 0;
+}
+  
+  
+  
