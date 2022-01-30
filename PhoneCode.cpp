87c24224865da9e8da74136e355996c63f31d44f
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-01-30 13:08:51 +0530

    Create PhoneCode.cpp

---
+++ PhoneCode.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <iosteram>
+int main (){
+  
+  int n;
+  scanf ("%d\n",&n);
+  
+   std::string first(""), newString("");
+  
+  
+   getline(std::cin, first);
+   
+  
+    int codeLength = first.size(), newLength = first.size();
+    
+    while(--n)
+    {
+    
+      getline(std::cin, newString);
+      
+      for(int index = codeLength - 1; index >= 0; index--){if(first[index] != newString[index]){newLength = index;}}
+      
+      codeLength = newLength;
+    }
+
+    printf("%d\n", codeLength);
+    
+  return 0;
+}
+  
+  
+  
