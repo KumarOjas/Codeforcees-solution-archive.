f920b0cf7e41be65c8f895ba6d2900732e1da9a6
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-02-05 15:25:17 +0530

    Create GoldenPlate.cpp

---
+++ GoldenPlate.cpp
@@ -0,0 +1,25 @@
+
+#include <cstdio>
+
+int main () {
+  
+  long w,h,k;
+ 
+  
+  scanf("%ld %ld %ld", &w, &h, &k);
+  
+  
+  long sum(0);
+  
+  while(k--)
+  
+  {
+    
+    sum += 2*(w + h) - 4; w -= 4; h -= 4;
+  
+  }
+  
+  printf("%ld\n", sum);
+
+    return 0;
+}
