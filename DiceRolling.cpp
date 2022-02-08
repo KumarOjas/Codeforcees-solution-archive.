117cf6af884282e0c26359a3d62100afdcc7f51d
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-02-08 18:46:41 +0530

    Create DiceRolling.cpp

---
+++ DiceRolling.cpp
@@ -0,0 +1,21 @@
+
+#include <cstdio>
+
+int main (){
+  
+  int t;
+
+   scanf ("%d",&t);
+  
+   while(t--){
+    
+    int x;
+   
+      scanf ("%d",&x);
+    
+     
+      printf("%d\n", x / 2);
+    }
+
+    return 0;
+}
