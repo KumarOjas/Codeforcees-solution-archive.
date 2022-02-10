d9b45f4e35965da5db3dcfa1535dffd511cc66c3
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-02-10 14:54:11 +0530

    Create KthSmallestElementinAbst.cpp

---
+++ KthSmallestElementinAbst.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  
+  
+  public :
+  
+  
+  int value = 0;
+  
+  int current = 0;
+  
+  bool done=0;
+  
+  
+  int kthSmallest (TreeNode* root, int k)
+  {
+  
+    current = k;
+    
+    getKthSmallest(root);
+    
+    return value;
+    
+  }
+    
+    void getKthSmallest(TreeNode* root)
+    {
+    
+      
+      
+      if(root == NULL){return;}
+      
+      
+      if(done)
+      {
+        return;
+      }
+    
+        getKthSmallest(root->left);
+        --current;
+        if(current == 0){value = root->val; done = 1; return;}
+        getKthSmallest(root->right);
+    }
+};
