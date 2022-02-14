8471fffb83f5cc73c9cfafed36ee54352eecee81
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-02-14 13:33:29 +0530

    Create LittleGirlAndMaximumXor.cpp

---
+++ LittleGirlAndMaximumXor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main (){
+
+int64_t a,b;
+std::cin >>a >>b;
+int64_t a,b;
+std::cin>>a>>b;
+int64_t d(1);
+while (d<=s){d*=2;}
+
+std::cout<<(d-1)<<std::endl;
+
+return 0;
+}
