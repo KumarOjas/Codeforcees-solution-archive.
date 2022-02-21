aa29f35255b5794a1191be94502591693584c490
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2022-02-21 18:42:14 +0530

    Create FindMaximum.cpp

---
+++ FindMaximum.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#incude <string>
+
+int main () {
+  
+  long n;
+  
+  scanf ("%lld\n",&n);
+  
+  
+  std::vector<long> a(n,0);
+   
+  
+  long ans(0), sum(0);
+  
+  
+  for(long p = 0; p < n; p++)
+  
+  {
+    scanf("%ld", &a[p]);
+    
+    sum += a[p];
+  }
+  
+  scanf("\n");
+  
+  std::string x; getline(std::cin, x);
+
+    for(long p = n - 1; p >= 0; p--){
+
+
+      if(x[p] == '1'){ans = (ans > (sum - a[p])) ? ans : (sum - a[p]);}
+      
+     
+      else{sum -= a[p];}
+    
+    }
+
+  
+    if(ans < sum){ans = sum;}
+
+  
+  printf("%ld\n", ans);
+
+  
+    return 0;
+}
+
+  
+  
